e2379a332fc0a338221516f7866c01db1d36fd4b
CodingAddict1530 <kejack2001@gmail.com>
2024-11-27 00:27:33 -0500

    Added license info

--- src/main/java/com/idk/agent/IDKAgent.java
+++ src/main/java/com/idk/agent/IDKAgent.java
@@ -1,3 +1,21 @@
+/*
+    IDKAgent - A Java Agent for advanced measurement of object memory footprints.
+    Copyright (C) 2024  Alexis Mugisha
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.idk.agent;
 
 import com.idk.classes.ReferenceSet;
